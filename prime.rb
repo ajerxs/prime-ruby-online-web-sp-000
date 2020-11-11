6abc6f7d7dc2031d4c25462e4e2a0a7b75d208c6
Andrew Eames <ajerxs@gmail.com>
2020-11-11 16:48:04 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,7 +2,7 @@ def prime?(number)
   if number <= 1
     return false
   elsif
-    (2..number - 1).each do |value|
+    (2..number - 1).each do |i|
       if value % i == 0
         return false
       end
